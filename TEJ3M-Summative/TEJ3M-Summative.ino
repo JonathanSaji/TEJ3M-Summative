d4fac11fc88342be9d6fdd573e4941d6fe717ece
Jonathan Saji <jonathansaji2009@gmail.com>
2026-01-21 17:54:44 -0500

    Serial added

--- TEJ3M-Summative/TEJ3M-Summative.ino
+++ TEJ3M-Summative/TEJ3M-Summative.ino
@@ -33,7 +33,6 @@ unsigned long bassNoteEndTime = 0;
 bool bassNoteActive = false;
 int bassNoteIndex = 0;
 
-
 // Buzzer PWM timing
 unsigned long lastMelodyToggleTime = 0;
 unsigned long lastBassToggleTime = 0;
@@ -46,25 +45,36 @@ long currentBassMicrosTime = 0;
 
 // Track active LED for proper on/off control
 int lastActiveLED = -1;
+bool playMusic = false;
 
+String lyrics[lyricsCount] = {"Happy Birthday", "To You", "Happy Birthday", "To You", "Happy Birthday", "Dear JAYDEN", "Happy Birthday", "To You!"};
+int songLength = 0;
+const int happyBirthdayLength = 25;
 
-String lyrics[lyricsCount] = {"Happy Birthday","To You","Happy Birthday","To You","Happy Birthday","Dear JAYDEN","Happy Birthday","To You!"};
-const int songLength = 25;
-int melodyNotes[songLength] = {NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_F4, NOTE_E4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4,
-                               NOTE_G4, NOTE_F4, NOTE_C4, NOTE_C4, NOTE_C5, NOTE_A4, NOTE_F4, NOTE_E4, NOTE_D4, NOTE_AS4,
-                               NOTE_AS4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_F4};
-int melodyDurations[songLength] = {3, 4, 2, 2, 2, 1, 3, 4, 2, 2, 2, 1, 3, 4, 2, 2, 2, 2, 2, 4, 4, 2, 2, 2, 1};
+// Happy Birthday Song
+int happyBirthdayMelody[25] = {NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_F4, NOTE_E4,
+                               NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_G4, NOTE_F4,
+                               NOTE_C4, NOTE_C4, NOTE_C5, NOTE_A4, NOTE_F4, NOTE_E4, NOTE_D4,
+                               NOTE_AS4, NOTE_AS4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_F4};
 
-int bassDurations[songLength] = {4, 4, 4, 4, 4, 4, 4, 4,4,
-                                 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-                                 4, 4, 4, 4, 4};
+int happyBirthdayDurations[25] = {3, 4, 2, 2, 2, 1, 3, 4, 2, 2, 2, 1, 3, 4, 2, 2, 2, 2, 2, 4, 4, 2, 2, 2, 1};
 
-int bassNotes[songLength] = {REST,REST,NOTE_F4,REST,REST,NOTE_C4,REST,REST,NOTE_C3,REST,
-                            REST,NOTE_F4,REST,REST,NOTE_F4,REST,REST,NOTE_AS4,REST,REST,
-                            REST,NOTE_F4,REST,NOTE_C4,NOTE_F4};
+int happyBirthdayBassDurations[25] = {4, 4, 4, 4, 4, 4, 4, 4, 4,
+                                      4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                                      4, 4, 4, 4, 4};
 
+int happyBirthdaybassNotes[25] = {REST, REST, NOTE_F4, REST, REST, NOTE_C4, REST, REST, NOTE_C3, REST,
+                                  REST, NOTE_F4, REST, REST, NOTE_F4, REST, REST, NOTE_AS4, REST, REST,
+                                  REST, NOTE_F4, REST, NOTE_C4, NOTE_F4};
+
+// Default empty arrays
+int melodyNotes[50] = {REST};
+int melodyDurations[50] = {REST};
+int bassDurations[50] = {REST};
+int bassNotes[50] = {REST};
 
 int noteDurationSignature = 1500;
+
 void setup() {
     Serial.begin(9600);
 
@@ -79,123 +89,154 @@ void setup() {
     lcd.init();
     lcd.backlight();
     lcd.clear();
-}   
+    playMenu();
+
+}
 
 void loop() {
     unsigned long currentTime = millis();
-    // Check if it's time to show the next message
-    if (currentTime - lastLyricsChangeTime >= currentDisplayInterval && lyricsIndex < lyricsCount) {
-    
-        // Loop back to start when reaching the end
-        if (lyricsIndex >= lyricsCount) {
+
+    // Check for serial input - read entire line until newline
+    if (Serial.available() > 0) {
+        String input = Serial.readStringUntil('\n');
+        input.trim(); // Remove any whitespace
+        
+        if (input == "1") {
+            for (int i = 0; i < happyBirthdayLength; i++) {
+                melodyNotes[i] = happyBirthdayMelody[i];
+                melodyDurations[i] = happyBirthdayDurations[i];
+                bassNotes[i] = happyBirthdaybassNotes[i];
+                bassDurations[i] = happyBirthdayBassDurations[i];
+            }
+            songLength = happyBirthdayLength;
+            playMusic = true;
             lyricsIndex = 0;
-        }
-
-        // Clear and display the new message
-        lcd.clear();
-        if (lyricsRow == 1) {
-            lcd.setCursor(0, 1);
-            lcd.print(lyrics[lyricsIndex]);
-            lyricsRow--;
-        }
-        else if (lyricsRow == 0) {
-            lcd.setCursor(0, 0);
-            lcd.print(lyrics[lyricsIndex]);
-            lcd.setCursor(0, 1);
-            // If it gets to the last lyric, loop back to the first
-            if (lyricsIndex == lyricsCount - 1) {
-                lcd.print(lyrics[0]);
-            }
-            else {
-                lcd.print(lyrics[lyricsIndex + 1]);
-            }
-            lyricsRow++;
-            lyricsIndex++;
-        }
-        if (lyricsDisplayCounter >= lyricsCount) {
+            lastLyricsChangeTime = currentTime;
             lyricsDisplayCounter = 0;
-        }
-        currentDisplayInterval = lyricsDisplayDuration[lyricsDisplayCounter++];
-        lastLyricsChangeTime = currentTime;
-    }
-
-    // Buzzer note handling
-    if (melodyNoteIndex < songLength) {
-        if (!melodyNoteActive) {
-            // Turn off previously active LED
-            if (lastActiveLED != -1) {
-                digitalWrite(lastActiveLED, HIGH);
-            }
-            
-            // Start new note and light up corresponding LED
-            switch (melodyNotes[melodyNoteIndex]) {
-                case NOTE_C4:
-                case NOTE_C5:
-                    digitalWrite(ledBlue, LOW);
-                    lastActiveLED = ledBlue;
-                    break;
-                case NOTE_D4:
-                    digitalWrite(ledYellow, LOW);
-                    lastActiveLED = ledYellow;
-                    break;
-                case NOTE_F4:
-                    digitalWrite(ledRed, LOW);
-                    lastActiveLED = ledRed;
-                    break;
-                case NOTE_G4:
-                case NOTE_E4:
-                    digitalWrite(ledGreen, LOW);
-                    lastActiveLED = ledGreen;
-                    break;
-                case NOTE_A4:
-                case NOTE_AS4:
-                    digitalWrite(ledWhite, LOW);
-                    lastActiveLED = ledWhite;
-                    break;
-                default:
-                    lastActiveLED = -1;
-                    break;
-            }
-            // Calculate half period for buzzer toggle (in microseconds)
-            melodyHalfPeriod = 1000000 / (melodyNotes[melodyNoteIndex] * 2);
-            bassHalfPeriod = 1000000 / (bassNotes[melodyNoteIndex] * 2);
-            lastMelodyToggleTime = micros();
-            lastBassToggleTime = micros();
-
-            melodyBuzzerState = false;
-            bassBuzzerState = false;
-
-            digitalWrite(melodyBuzzer, LOW);
-            digitalWrite(bassBuzzer, LOW);
-
-            melodyNoteEndTime = currentTime + (noteDurationSignature / melodyDurations[melodyNoteIndex]);
-            melodyNoteActive = true;
-
-            bassNoteEndTime = currentTime + (noteDurationSignature / bassDurations[melodyNoteIndex]);
-            bassNoteActive = true;
-        }
-        
-        // Toggle buzzer pins for continuous tone
-        currentMicrosTime = micros();
-        playMelody(melodyHalfPeriod, melodyBuzzer);
-        playBass(bassHalfPeriod, bassBuzzer);
-        
-        if (currentTime >= melodyNoteEndTime) {
-            // Note time is done - turn off the LED that was on
-            if (lastActiveLED != -1) {
-                digitalWrite(lastActiveLED, HIGH);
-            }
-            digitalWrite(melodyBuzzer, LOW);
-            digitalWrite(bassBuzzer, LOW);
-            melodyNoteIndex++;
-            bassNoteIndex++;
-            melodyNoteActive = false;
-            bassNoteActive = false;
+            melodyNoteIndex = 0;
+            bassNoteIndex = 0;
+        } else {
+            lcd.clear();
+            lcd.print("Invalid Option");
+            delay(2000);
+            lcd.clear();
+            playMenu();
         }
     }
-    else {
-        delay(1000); // Wait a second before restarting
-        lcd.clear();
+
+    if (playMusic) {
+        if (currentTime - lastLyricsChangeTime >= currentDisplayInterval && lyricsIndex < lyricsCount) {
+
+            // Loop back to start when reaching the end
+            if (lyricsIndex >= lyricsCount) {
+                lyricsIndex = 0;
+            }
+
+            // Clear and display the new message
+            lcd.clear();
+            if (lyricsRow == 1) {
+                lcd.setCursor(0, 1);
+                lcd.print(lyrics[lyricsIndex]);
+                lyricsRow--;
+            } else if (lyricsRow == 0) {
+                lcd.setCursor(0, 0);
+                lcd.print(lyrics[lyricsIndex]);
+                lcd.setCursor(0, 1);
+                // If it gets to the last lyric, loop back to the first
+                if (lyricsIndex == lyricsCount - 1) {
+                    lcd.print(lyrics[0]);
+                } else {
+                    lcd.print(lyrics[lyricsIndex + 1]);
+                }
+                lyricsRow++;
+                lyricsIndex++;
+            }
+            if (lyricsDisplayCounter >= lyricsCount) {
+                lyricsDisplayCounter = 0;
+            }
+            currentDisplayInterval = lyricsDisplayDuration[lyricsDisplayCounter++];
+            lastLyricsChangeTime = currentTime;
+        }
+
+        // Buzzer note handling
+        if (melodyNoteIndex < songLength) {
+            if (!melodyNoteActive) {
+                // Turn off previously active LED
+                if (lastActiveLED != -1) {
+                    digitalWrite(lastActiveLED, HIGH);
+                }
+
+                // Start new note and light up corresponding LED
+                switch (melodyNotes[melodyNoteIndex]) {
+                    case NOTE_C4:
+                    case NOTE_C5:
+                        digitalWrite(ledBlue, LOW);
+                        lastActiveLED = ledBlue;
+                        break;
+                    case NOTE_D4:
+                        digitalWrite(ledYellow, LOW);
+                        lastActiveLED = ledYellow;
+                        break;
+                    case NOTE_F4:
+                        digitalWrite(ledRed, LOW);
+                        lastActiveLED = ledRed;
+                        break;
+                    case NOTE_G4:
+                    case NOTE_E4:
+                        digitalWrite(ledGreen, LOW);
+                        lastActiveLED = ledGreen;
+                        break;
+                    case NOTE_A4:
+                    case NOTE_AS4:
+                        digitalWrite(ledWhite, LOW);
+                        lastActiveLED = ledWhite;
+                        break;
+                    default:
+                        lastActiveLED = -1;
+                        break;
+                }
+                // Calculate half period for buzzer toggle (in microseconds)
+                melodyHalfPeriod = 1000000 / (melodyNotes[melodyNoteIndex] * 2);
+                bassHalfPeriod = 1000000 / (bassNotes[melodyNoteIndex] * 2);
+                lastMelodyToggleTime = micros();
+                lastBassToggleTime = micros();
+
+                melodyBuzzerState = false;
+                bassBuzzerState = false;
+
+                digitalWrite(melodyBuzzer, LOW);
+                digitalWrite(bassBuzzer, LOW);
+
+                melodyNoteEndTime = currentTime + (noteDurationSignature / melodyDurations[melodyNoteIndex]);
+                melodyNoteActive = true;
+
+                bassNoteEndTime = currentTime + (noteDurationSignature / bassDurations[melodyNoteIndex]);
+                bassNoteActive = true;
+            }
+
+            // Toggle buzzer pins for continuous tone
+            currentMicrosTime = micros();
+            playMelody(melodyHalfPeriod, melodyBuzzer);
+            playBass(bassHalfPeriod, bassBuzzer);
+
+            if (currentTime >= melodyNoteEndTime) {
+                // Note time is done - turn off the LED that was on
+                if (lastActiveLED != -1) {
+                    digitalWrite(lastActiveLED, HIGH);
+                }
+                digitalWrite(melodyBuzzer, LOW);
+                digitalWrite(bassBuzzer, LOW);
+                melodyNoteIndex++;
+                bassNoteIndex++;
+                melodyNoteActive = false;
+                bassNoteActive = false;
+            }
+        } else {
+            delay(1000); // Wait a second before restarting
+            lcd.clear();
+            playMusic = false;
+            playMenu();
+        }
     }
 }
 
@@ -207,7 +248,6 @@ void playMelody(long halfPeriod, int buzzerPin) {
     }
 }
 
-
 void playBass(long halfPeriod, int buzzerPin) {
     currentBassMicrosTime = micros();
     if (currentBassMicrosTime - lastBassToggleTime >= halfPeriod) {
@@ -217,19 +257,17 @@ void playBass(long halfPeriod, int buzzerPin) {
     }
 }
 
-
-
-
+void playMenu() {
+    lcd.setCursor(0, 0);
+    lcd.print("1=HBD");
+}
 
 /*
-
-NOTES
-
-From https://docs.arduino.cc/language-reference/en/functions/advanced-io/tone/ 
-"If you want to play different pitches on multiple pins, you need to call noTone() 
-  on one pin before calling tone() on the next pin."
-  
-  */
+ * NOTES
+ * From https://docs.arduino.cc/language-reference/en/functions/advanced-io/tone/
+ * "If you want to play different pitches on multiple pins, you need to call noTone()
+ *  on one pin before calling tone() on the next pin."
+ */
 
 
 
